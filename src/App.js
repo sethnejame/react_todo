853002a198ac686bb0ee913d86572387f8315aff
Seth NeJame <sethnejame@gmail.com>
2019-06-26 15:02:15 +0200

    Adding working checkboxes & styling

--- src/App.js
+++ src/App.js
@@ -23,11 +23,20 @@ class App extends Component {
       }
     ]
   }
+
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id == id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })});
+  }
+
   render() {
-    console.log(this.state.todos)
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
   render() {
     return this.props.todos.map((listItem) => (
-      <TodoItem todo={listItem}/>
+      <TodoItem key={listItem.id} todo={listItem} markComplete=
+        {this.props.markComplete} />
     ));
   }
+}
+
+
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
 }
 
 export default Todos;
