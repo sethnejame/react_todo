b39345c51801db57baeebce44bad99d5077f747b
Seth NeJame <sethnejame@gmail.com>
2019-06-26 12:41:03 +0200

    Adding state to App component (todos)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import './App.css';
 
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Feed the obese bastard, Fat Bob',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Cook meals for the week (Sunday)',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Get a junior developer job',
+        completed: false
+      }
+    ]
+  }
   render() {
+    console.log(this.state.todos)
     return (
       <div className="App">
         <Todos />
