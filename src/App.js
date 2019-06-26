4ba495d62490f5470aa7e260ba29d29e760bec98
Seth NeJame <sethnejame@gmail.com>
2019-06-26 12:57:01 +0200

    Creating TodoItem component, linking it to Todos.js, output to browser works

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
     console.log(this.state.todos)
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
