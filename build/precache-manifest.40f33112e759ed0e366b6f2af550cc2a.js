5308a0905464a71de79c92dc904097ceaee50d75
Seth NeJame <sethnejame@gmail.com>
2019-06-27 11:22:13 +0200

    Deploying npm build

---
+++ build/precache-manifest.40f33112e759ed0e366b6f2af550cc2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3243a9a4ef9e2ecebbcf06020acceb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b9baa731690f514bc37",
+    "url": "/static/css/main.0d6a07f3.chunk.css"
+  },
+  {
+    "revision": "5acfd800d23e924fe964",
+    "url": "/static/js/2.7fa37ec6.chunk.js"
+  },
+  {
+    "revision": "4b9baa731690f514bc37",
+    "url": "/static/js/main.046f32ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
